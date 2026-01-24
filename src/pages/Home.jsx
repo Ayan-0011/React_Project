9910ea7abaf353bea34a0ff338e9dcdc1fd8cefa
Ayan_0011 <ayanansari9235@gmail.com>
2026-01-24 10:56:05 +0530

    lazy loading applyed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import Crousel from '../Components/Crousel'
+import React, { lazy, Suspense } from 'react'
 import Midbanner from '../Components/Midbanner'
 import Features from '../Components/Features'
 import FeaturedProducts from '../pages/FeatureProdcuts'
 import CategoryBanners from './CategoryBanners'
 
+import Loading from '../assets/Loading4.webm'
+
 const Home = () => {
+
+  const Crousel = lazy(() => import('../Components/Crousel'))
+
   return (
     <>
-      <Crousel/>
-      <CategoryBanners/>
-      <FeaturedProducts/>
-      <Midbanner/>
-      <Features/>
+
+      <Suspense fallback={<> <div className='flex justify-center items-center w-[400px] mx-auto'>
+        <video muted autoPlay loop>
+          <source src={Loading} type='video/webm' />
+        </video>
+      </div></>}>
+      
+        <Crousel />
+
+        <CategoryBanners />
+        <FeaturedProducts />
+      </Suspense>
+      <Midbanner />
+      <Features />
     </>
 
-    
+
   )
 }
 
